03478f7931f51dd8614d01a6a9d17f5878a3d1d0
Firmansyah Helmi Kurniawan <kurniawan.firman94@gmail.com>
2021-12-20 09:53:41 +0700

    Update config.py

--- moodle/config.py
+++ moodle/config.py
@@ -1,14 +1,14 @@
 class loginData(object):
     server = True
-    username = ""  # moodle login username or email
-    password = ""  # moodle login password
-    telgramtoken = ""  # telegram bot token from @botfather
+    username = "2007421030"  # moodle login username or email
+    password = "Y5hZ6sJ6"  # moodle login password
+    telgramtoken = "5046088120:AAGYVsLwLNHu4hLgS4jlySRtegSF70quH1Y"  # telegram bot token from @botfather
     chatid = (
-        ""  # Your unique telegram chat_id or @username (settings->copy id or username)
+        "@FIRMAN_KURNIAWAN"  # Your unique telegram chat_id or @username (settings->copy id or username)
     )
     totalcourse = 8
     # Select your semester from http://moodle.mec.ac.in/course/index.php?categoryid=4,
     # then check url for semid  eg:moodle.mec/../categoryid=28( "28"categoryid is the semid)
-    moodleSemId = "28"
-    startTime = "02:01"  # IST 12 hour
-    endTime = "10:00"  # IST 12 hour
+    moodleSemId = "525"
+    startTime = "01:00"  # IST 12 hour
+    endTime = "02:00"  # IST 12 hour
